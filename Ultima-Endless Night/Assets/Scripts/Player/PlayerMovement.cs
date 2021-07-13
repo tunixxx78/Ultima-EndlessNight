8774453de657ef786609fc89a6fd40ed7357a263
tunixxx78 <73437970+tunixxx78@users.noreply.github.com>
2021-07-13 15:36:56 +0300

    First test for movement(player)

---
+++ Ultima-Endless Night/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed = 1f;
+    [SerializeField] Rigidbody rbPlayer;
+    private Vector3 movement;
+
+    private void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer(movement);
+    }
+
+    public void MovePlayer(Vector3 direction) 
+    {
+        rbPlayer.MovePosition(transform.position + (direction * speed * Time.deltaTime));
+    }
+}
